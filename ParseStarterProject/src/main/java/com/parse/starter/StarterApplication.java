2253ca8e9721f57f4c6e855d555ffd5f92b22f18
Suyog Naik <suyognaik628@gmail.com>
2020-07-10 00:05:23 +0530

    simple Messaging App

--- ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -30,9 +30,9 @@ public class StarterApplication extends Application {
 
     // Add your initialization code here
     Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-            .applicationId("")
-            .clientKey("")
-            .server("")
+            .applicationId("myappID")
+            .clientKey("KBlXcGVsRCL1")
+            .server("http://18.188.177.125/parse/")
             .build()
     );
 
